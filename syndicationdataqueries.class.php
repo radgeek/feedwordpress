fbf0b31061b36852c7ef95a6f1bf166b9544f88d
radgeek <radgeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-31 18:38:29 +0000

    Guess we'll need this.
    git-svn-id: http://plugins.svn.wordpress.org/feedwordpress/trunk@391395 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ syndicationdataqueries.class.php
@@ -0,0 +1,69 @@
+<?php
+class SyndicationDataQueries {
+	function SyndicationDataQueries () {
+		add_action('init', array(&$this, 'init'));
+		add_filter('posts_search', array(&$this, 'posts_search'), 10, 2);
+		add_filter('posts_fields', array(&$this, 'posts_fields'), 10, 2);
+		add_filter('posts_request', array(&$this, 'posts_request'), 10, 2);
+	}
+
+	function init () {
+		global $wp;
+		$wp->add_query_var('guid');
+	}
+
+	function posts_request ($sql, &$query) {
+		if ($query->get('fields') == '_synfresh') :
+			FeedWordPress::diagnostic('feed_items:freshness:sql', "SQL: ".$sql);
+		endif;
+		return $sql;
+	}
+	
+	function posts_search ($search, &$query) {
+		global $wpdb;
+		if ($guid = $query->get('guid')) :
+			if (strlen(trim($guid)) > 0) :
+				$seek = array($guid);
+				$md5Seek = array();
+				
+				// MD5 hashes
+				if (preg_match('/^[0-9a-f]{32}$/i', $guid)) :
+					$md5Seek = array($guid);
+					$seek[] = SyndicatedPost::normalize_guid_prefix().$guid;
+				endif;
+
+				// URLs that are invalid, or that WordPress just doesn't like
+				$nGuid = SyndicatedPost::normalize_guid($guid);
+				if ($guid != $nGuid) :
+					$seek[] = $nGuid;
+				endif;
+				
+				// Assemble
+				$guidMatch = "(guid = '".implode("') OR (guid = '", $seek)."')";
+				if (count($md5Seek) > 0) :
+					$guidMatch .= " OR (MD5(guid) = '".implode("') OR (MD5(guid) = '", $md5Seek)."')";
+				endif;
+				
+				$search .= $wpdb->prepare(" AND ($guidMatch)");
+			endif;
+		endif;
+		return $search;
+	} /* SyndicationDataQueries::posts_where () */
+	
+	function posts_fields ($fields, &$query) {
+		global $wpdb;
+		if ($f = $query->get('fields')) :
+			switch ($f) :
+			case '_synfresh' :
+				$fields = "{$wpdb->posts}.ID, {$wpdb->posts}.guid, {$wpdb->posts}.post_modified_gmt";
+				break;
+			default :
+				// Do nothing.
+			endswitch;
+		endif;
+		return $fields;
+	} /* SyndicationDataQueries::posts_fields () */
+}
+
+$SDQ = new SyndicationDataQueries;
+
