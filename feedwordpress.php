9522b94633d91013f0a0dd2a5bd61ccfddf197fc
radgeek <radgeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-24 12:52:28 +0000

    Dev ver bump.
    git-svn-id: http://plugins.svn.wordpress.org/feedwordpress/trunk@455036 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- feedwordpress.php
+++ feedwordpress.php
@@ -3,7 +3,7 @@
 Plugin Name: FeedWordPress
 Plugin URI: http://feedwordpress.radgeek.com/
 Description: simple and flexible Atom/RSS syndication for WordPress
-Version: 2011.1019
+Version: 2011.10124
 Author: Charles Johnson
 Author URI: http://radgeek.com/
 License: GPL
@@ -11,7 +11,7 @@ License: GPL
 
 /**
  * @package FeedWordPress
- * @version 2011.1019
+ * @version 2011.1024
  */
 
 # This uses code derived from:
@@ -34,7 +34,7 @@ License: GPL
 
 # -- Don't change these unless you know what you're doing...
 
-define ('FEEDWORDPRESS_VERSION', '2011.1019');
+define ('FEEDWORDPRESS_VERSION', '2011.1024');
 define ('FEEDWORDPRESS_AUTHOR_CONTACT', 'http://radgeek.com/contact');
 
 if (!defined('FEEDWORDPRESS_BLEG')) :
