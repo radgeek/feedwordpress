8e212bf6bfd6ba03168bdd432e70789bc812f53e
Rad Geek <feedback+finarfin@radgeek.com>
2017-10-28 14:29:05 +0000

    Ver bump.

--- feedwordpress.php
+++ feedwordpress.php
@@ -3,7 +3,7 @@
 Plugin Name: FeedWordPress
 Plugin URI: http://feedwordpress.radgeek.com/
 Description: simple and flexible Atom/RSS syndication for WordPress
-Version: 2017.1020
+Version: 2017.1028
 Author: C. Johnson
 Author URI: https://feedwordpress.radgeek.com/contact/
 License: GPL
@@ -11,22 +11,25 @@ License: GPL
 
 /**
  * @package FeedWordPress
- * @version 2017.1020
+ * @version 2017.1028
  */
 
-# This uses code derived from:
+# This plugin uses code derived from:
 # -	wp-rss-aggregate.php by Kellan Elliot-McCrea <kellan@protest.net>
 # -	SimplePie feed parser by Ryan Parman, Geoffrey Sneddon, Ryan McCue, et al.
 # -	MagpieRSS feed parser by Kellan Elliot-McCrea <kellan@protest.net>
 # -	Ultra-Liberal Feed Finder by Mark Pilgrim <mark@diveintomark.org>
 # -	WordPress Blog Tool and Publishing Platform <http://wordpress.org/>
+# -	Github contributors @Flynsarmy, @BandonRandon, @david-robinson-practiceweb,
+# 	@daidais, @thegreatmichael, @stedaniels, @alexiskulash, @quassy, @zoul0813,
+# 	@timmmmyboy, and @vobornik
 # according to the terms of the GNU General Public License.
 
 ####################################################################################
 ## CONSTANTS & DEFAULTS ############################################################
 ####################################################################################
 
-define ('FEEDWORDPRESS_VERSION', '2017.1020');
+define ('FEEDWORDPRESS_VERSION', '2017.1028');
 define ('FEEDWORDPRESS_AUTHOR_CONTACT', 'http://feedwordpress.radgeek.com/contact');
 
 if (!defined('FEEDWORDPRESS_BLEG')) :
