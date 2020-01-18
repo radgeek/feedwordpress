fddefa2a995672c47a65e66955646bc9ed8484b0
Rad Geek <feedback+finarfin@radgeek.com>
2020-01-18 23:34:37 +0000

    2020.0118 / incorporate code tweaks up to present and first successful compatibility testing with WP 5.x

--- feedwordpress.php
+++ feedwordpress.php
@@ -3,7 +3,7 @@
 Plugin Name: FeedWordPress
 Plugin URI: http://feedwordpress.radgeek.com/
 Description: simple and flexible Atom/RSS syndication for WordPress
-Version: 2018.0117
+Version: 2020.0118
 Author: C. Johnson
 Author URI: https://feedwordpress.radgeek.com/contact/
 License: GPL
@@ -11,7 +11,7 @@ License: GPL
 
 /**
  * @package FeedWordPress
- * @version 2018.0117
+ * @version 2020.0118
  */
 
 # This plugin uses code derived from:
@@ -29,7 +29,7 @@ License: GPL
 ## CONSTANTS & DEFAULTS ############################################################
 ####################################################################################
 
-define ('FEEDWORDPRESS_VERSION', '2018.0117');
+define ('FEEDWORDPRESS_VERSION', '2020.0118');
 define ('FEEDWORDPRESS_AUTHOR_CONTACT', 'http://feedwordpress.radgeek.com/contact');
 
 if (!defined('FEEDWORDPRESS_BLEG')) :
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: C. Johnson
 Donate link: http://feedwordpress.radgeek.com/donate/
 Tags: syndication, aggregation, feed, atom, rss
 Requires at least: 4.5
-Tested up to: 4.8.2
-Stable tag: 2017.1020
+Tested up to: 5.3.2
+Stable tag: 2020.0118
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
