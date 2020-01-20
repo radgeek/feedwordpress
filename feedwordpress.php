2c840d15550dafa282771d5d908925774196b885
Rad Geek <feedback+finarfin@radgeek.com>
2020-01-20 21:52:28 +0000

    Ver bump.

--- feedwordpress.php
+++ feedwordpress.php
@@ -3,7 +3,7 @@
 Plugin Name: FeedWordPress
 Plugin URI: http://feedwordpress.radgeek.com/
 Description: simple and flexible Atom/RSS syndication for WordPress
-Version: 2020.0119
+Version: 2020.0120
 Author: C. Johnson
 Author URI: https://feedwordpress.radgeek.com/contact/
 License: GPL
@@ -11,7 +11,7 @@ License: GPL
 
 /**
  * @package FeedWordPress
- * @version 2020.0119
+ * @version 2020.0120
  */
 
 # This plugin uses code derived from:
@@ -22,14 +22,15 @@ License: GPL
 # -	WordPress Blog Tool and Publishing Platform <http://wordpress.org/>
 # -	Github contributors @Flynsarmy, @BandonRandon, @david-robinson-practiceweb,
 # 	@daidais, @thegreatmichael, @stedaniels, @alexiskulash, @quassy, @zoul0813,
-# 	@timmmmyboy, @vobornik, and @inanimatt
+# 	@timmmmyboy, @vobornik, @inanimatt, @tristanleboss, @martinburchell,
+#   and @oppiansteve 
 # according to the terms of the GNU General Public License.
 
 ####################################################################################
 ## CONSTANTS & DEFAULTS ############################################################
 ####################################################################################
 
-define ('FEEDWORDPRESS_VERSION', '2020.0119');
+define ('FEEDWORDPRESS_VERSION', '2020.0120');
 define ('FEEDWORDPRESS_AUTHOR_CONTACT', 'http://feedwordpress.radgeek.com/contact');
 
 if (!defined('FEEDWORDPRESS_BLEG')) :
