f28f590f01eaa635d90d72d21b57cc70cad1c87a
Charles Johnson <feedback+github@radgeek.com>
2020-08-22 17:35:41 +0000

    Reorganization - create assets/images/, templates/

---
+++ templates/notice-debug-mode-yes.html.php
@@ -0,0 +1,7 @@
+<div class="error">
+<p><strong>FeedWordPress warning.</strong> Debugging mode is <strong>ON</strong>.
+While it remains on, FeedWordPress displays many diagnostic error messages,
+warnings, and notices that are ordinarily suppressed, and also turns off all
+caching of feeds. Use with caution: this setting is absolutely inappropriate
+for a production server.</p>
+</div>
